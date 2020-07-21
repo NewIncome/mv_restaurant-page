978c8dd18a7d78d3507bd5ed1dd0958259189df7
NewIncome <alfredo_cardenas@live.com>
2020-07-21 11:24:42 -0500

    Modify code to export an init function to be called in the index.js to then populate all the html.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import './style.scss';
-import './init-pg-ld.js';
+import { initPageLoad, crtDOM } from './init-pg-ld.js';
 
 console.log('NPM and basic setup test; log message');
 
-['a', 'b', 'c'].forEach((e, i) => {
-  // this["let " + `var-${e}`] = 1 + 1;
-  eval("let var_" + e + "=" + i);
-});
+initPageLoad();
--- src/init-pg-ld.js
+++ src/init-pg-ld.js
@@ -2,10 +2,10 @@
 console.log("in 2nd file");
 
 // ----- Factory Function to make DOM elements -----
-function crtDOM(type) {
-  dom = document.createElement(type)
+export function crtDOM(type) {
+  let dom = document.createElement(type)
   const mkChild = (parent) => { document.querySelector(parent).appendChild(dom) }
-  const addAttribute = (att, val) => { this.dom.setAttribute(att, val) }
+  const addAttribute = (att, val) => { dom.setAttribute(att, val) }
   const mkChildNattribute = (parent, att, val) => {
     mkChild(parent)
     addAttribute(att, val)
@@ -19,16 +19,19 @@ function crtDOM(type) {
 };
 
 // Make the DOM elements from the crtDOM Factory Function
-const nav = crtDOM('nav').mkChildNattribute('#content', 'id', 'header-nav');
-const navLogo = crtDOM('div').mkChildNattNtext('#header-nav', 'class', 'logo', 'Baratie');
-const navUL = crtDOM('ul').mkChildNattribute('#header-nav', 'class', 'links');
-['Home', 'Menu', 'Contact'].forEach((e, i) => {
-  eval(`nli${i}` + '=' + crtDOM('li').mkChildNattribute('.links', 'class', `n-li nli-${i}`))
-  crtDOM('a').mkChildNattNtext(`.nli-${i}`, 'class', `n-link n-a-${i}`, e)
-  document.querySelector(`.n-a-${i}`).setAttribute('href', '#') });
-const headline = crtDOM('div').mkChildNattribute('#content', 'id', 'headline');
-const hlH2 = crtDOM('h2').mkChildNattNtext('#headline', 'class', 'headline-h', 'The best restaurant in town');
-const hlP = crtDOM('p').mkChildNattNtext('#headline', 'class', 'headline-p', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.');
+export function initPageLoad() {
+  const nav = crtDOM('nav').mkChildNattribute('#content', 'id', 'header-nav');
+  const navLogo = crtDOM('div').mkChildNattNtext('#header-nav', 'class', 'logo', 'Baratie');
+  const navUL = crtDOM('ul').mkChildNattribute('#header-nav', 'class', 'links');
+  ['Home', 'Menu', 'Contact'].forEach((e, i) => {
+    eval(`let nli${i}` + '=' + crtDOM('li').mkChildNattribute('.links', 'class', `n-li nli-${i}`))
+    crtDOM('a').mkChildNattNtext(`.nli-${i}`, 'class', `n-link n-a-${i}`, e)
+    document.querySelector(`.n-a-${i}`).setAttribute('href', '#') });
+  const headline = crtDOM('div').mkChildNattribute('#content', 'id', 'headline');
+  const hlH2 = crtDOM('h2').mkChildNattNtext('#headline', 'class', 'headline-h', 'The best restaurant in town');
+  const hlP = crtDOM('p').mkChildNattNtext('#headline', 'class', 'headline-p', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.');
+};
+
 
 // // Module Pattern  ;  for 1 occurrence
 // const calculator = (() => {
