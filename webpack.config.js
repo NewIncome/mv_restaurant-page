44e502c6524b0023f0a87f30a34e568483f22980
NewIncome <alfredo_cardenas@live.com>
2020-07-22 12:59:09 -0500

    Fix linter errors for webpack.config file

--- webpack.config.js
+++ webpack.config.js
@@ -8,18 +8,18 @@ module.exports = {
         test: /\.s[ac]ss$/,
         use: [
           'style-loader', // Creates 'style' nodes from JS strings
-          'css-loader',   // Translates CSS into CommonJS
-          'sass-loader'   // Compiles Sass to CSS
-        ]
+          'css-loader', // Translates CSS into CommonJS
+          'sass-loader', // Compiles Sass to CSS
+        ],
       },
       {
         test: /\.(png|svg|jpe?g|gif)$/,
-        use: ['file-loader']
-      }
-    ]
+        use: ['file-loader'],
+      },
+    ],
   },
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
-  }
-}
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
